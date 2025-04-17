96e2d62449d80769400c2d0e97bb2c9af141eeb0
Jerry <156297470+J9-239717@users.noreply.github.com>
2025-04-17 12:52:16 +0700

    Add CSR and CSC matrix structures and conversion functions; update .gitignore

--- sparse_matrix.h
+++ sparse_matrix.h
@@ -12,5 +12,18 @@ void printSparseMatrix(sparse_matrix* matrix);
 void freeSparseMatrix(sparse_matrix* matrix);
 sparse_matrix* sparese_row_wise(sparse_matrix* matrix,float* vector,int size_vector);
 void print_SP_Matrix_to_file(sparse_matrix* matrix, const char* filename,word_hash* vocab);
+csr_matrix* to_csr(sparse_matrix* sm);
+csc_matrix* to_csc(sparse_matrix* sm);
+csr_matrix* csr_transpose(csr_matrix* ori);
+void free_csr_matrix(csr_matrix* csr);
+void free_csc_matrix(csc_matrix* csc);
+float** csr_to_dense(csr_matrix* csr);
+float** csc_to_dense(csc_matrix* csc);
+void free_dense_matrix(float** dense, int rows);
+void print_dense_matrix(float** dense, int rows, int cols);
+void print_csc(csc_matrix* csc);
+void print_csr(csr_matrix* csr);
+csc_matrix* covert_to_csc(csr_matrix* ori);
+float **csr_x_csc_to_dense(const csr_matrix *A, const csc_matrix *B);
 
 #endif
